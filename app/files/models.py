c18a193e68ee3f64e88516b3dc7795f7233d5deb
Dratyti <belousovi783@gmail.com>
2024-06-17 11:44:17 +0300

    время создания в виде unix

--- app/files/models.py
+++ app/files/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,8 +7,9 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Tags(MPTTModel):
-    time_create = models.DateTimeField(
-        auto_now_add=True, verbose_name='Время добавления')
+    time_create = models.BigIntegerField(
+        verbose_name='Время добавления', blank=True
+    )
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Создатель')
     section = models.BooleanField(default=False, verbose_name='Раздел')
     name = models.CharField(max_length=150, unique=True, verbose_name='Название категории')
@@ -31,6 +34,11 @@ class Tags(MPTTModel):
     def __str__(self):
         return f"{self.name}"
 
+    def save(self, *args, **kwargs):
+        if not self.time_create:
+            self.time_create = int(datetime.now().timestamp())
+        super().save(*args, **kwargs)
+
 
 class Material(models.Model):
     name = models.CharField(max_length=500, unique=True, verbose_name="Название")
@@ -46,8 +54,9 @@ class Material(models.Model):
 
 
 class File(models.Model):
-    time_create = models.DateTimeField(
-        auto_now_add=True, verbose_name='Время добавления')
+    time_create = models.BigIntegerField(
+        verbose_name='Время добавления', blank=True
+    )
     filename = models.CharField(max_length=500, unique=True, verbose_name="Имя файла")
     owners = models.ManyToManyField(to=User, verbose_name='Владельцы')
     height = models.FloatField(verbose_name='Высота')
@@ -75,10 +84,16 @@ class File(models.Model):
     def __str__(self):
         return self.filename
 
+    def save(self, *args, **kwargs):
+        if not self.time_create:
+            self.time_create = int(datetime.now().timestamp())
+        super().save(*args, **kwargs)
+
 
 class DescriptionFile(models.Model):
-    time_create = models.DateTimeField(
-        auto_now_add=True, verbose_name='Время добавления')
+    time_create = models.BigIntegerField(
+        verbose_name='Время добавления', blank=True
+    )
     file = models.ForeignKey(File, on_delete=models.CASCADE, related_name='description_file', verbose_name='File')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
     title = models.CharField(max_length=300, verbose_name='Заголовок')
@@ -98,6 +113,11 @@ class DescriptionFile(models.Model):
 
     def __str__(self):
         return f"{self.file.filename}-{self.title}"
+
+    def save(self, *args, **kwargs):
+        if not self.time_create:
+            self.time_create = int(datetime.now().timestamp())
+        super().save(*args, **kwargs)
 
 
 class ImageFile(models.Model):
