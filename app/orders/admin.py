e7cfcb964cdad100f98398e10b04e0464168d2b5
dratyti22 <belousovi783@gmail.com>
2024-07-16 22:55:23 +0300

    create model Customer

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["customer_id", "delivery_type"]
+    list_filter = ["customer_id", "delivery_type"]
--- app/orders/models.py
+++ app/orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    DELIVERY_TYPE = [
+        (1, "Самовывоз"),
+        (2, "Доставка на адрес"),
+    ]
+    customer_id = models.IntegerField(verbose_name="ID заказчика")
+    delivery_type = models.IntegerField(choices=DELIVERY_TYPE, verbose_name="Тип доставки")
+
+    street = models.CharField(max_length=255, verbose_name="Улица")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    postal_code = models.CharField(max_length=10, verbose_name="Индекс")
+
+    delivery_deadline = models.TextField(verbose_name="Сроки доставки")
+    self_pickup_hours = models.TextField(null=True, blank=True, verbose_name="Когда работает")
+
+    class Meta:
+        db_table = "order_customer"
+        verbose_name = 'Заказчик'
+        verbose_name_plural = "Заказчики"
+
+    def __str__(self):
+        return f"{self.customer_id} - {self.delivery_type}"
+
