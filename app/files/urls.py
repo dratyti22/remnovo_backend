162171b122a4fe24d163821c653e552574854edc
dratyti22 <belousovi783@gmail.com>
2024-07-16 21:41:32 +0300

    исправленные пути

--- app/files/urls.py
+++ app/files/urls.py
@@ -1,5 +1,3 @@
-from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import SimpleRouter
 
 from . import views
@@ -12,8 +10,7 @@ router.register('api/material', views.MaterialView)
 router.register('api/description/file', views.DescriptionFileView)
 
 urlpatterns = [
-    path('api/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+
 ]
 
 urlpatterns += router.urls
