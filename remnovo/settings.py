01526ee0d946c1c6f8583b4f8c72a960f9287664
dratyti22 <belousovi783@gmail.com>
2024-07-19 12:36:01 +0300

    создан logging

--- remnovo/settings.py
+++ remnovo/settings.py
@@ -157,6 +157,7 @@ INTERNAL_IPS = [
 
 LOGGING = {
     'version': 1,
+    'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
@@ -167,15 +168,41 @@ LOGGING = {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': 'docker/logs/app.log',
-            'maxBytes': 1024 * 1024 * 5,  # 5MB
+            'maxBytes': 1024 * 1024 * 5,  # 6MB
             'backupCount': 5,
             'formatter': 'verbose',
         },
+        'debug_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'docker/logs/debug.log',
+            'maxBytes': 1024 * 1024 * 4,  # 5MB
+            'backupCount': 4,
+            'formatter': 'verbose',
+        },
+        'error_file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'docker/logs/error.log',
+            'maxBytes': 1024 * 1024 * 6,  # 5MB
+            'backupCount': 5,
+            'formatter': 'verbose',
+        }
     },
     'loggers': {
         'django.db.backends': {
             'handlers': ['file'],
             'level': "INFO"
+        },
+        'django': {
+            'handlers': ['debug_file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'root': {
+            'handlers': ['error_file'],
+            'level': 'ERROR',
+            'propagate': True,
         },
     }
 }
@@ -185,12 +212,12 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_RENDERER_CLASSES': (
-    #     'rest_framework.renderers.JSONRenderer',
-    # ),
-    # 'DEFAULT_PARSER_CLASSES': (
-    #     'rest_framework.parsers.JSONParser',
-    # ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
 
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #
