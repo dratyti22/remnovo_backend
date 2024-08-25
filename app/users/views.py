150094bc386a5a170c4422dfdd79776b117306bf
dratyti22 <belousovi783@gmail.com>
2024-08-25 20:56:08 +0300

    создание пользователя

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from app.users.models import CustomUser
+
+
+class CreateUserView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        if not email or not password:
+            return Response({'error': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        CustomUser.objects.create_user(email=email, password=password)
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+
+
+class LoginUserView(APIView):
+    ...
+
+
+class AuthTokenView(APIView):
+    ...
+
+
+class EmailConfirmationView(APIView):
+    ...
