d5a77550e288cba003aeb671fee2cf67a25c3bf9
dratyti22 <belousovi783@gmail.com>
2024-08-26 21:07:15 +0300

    сделано вход по token или email

--- app/users/views.py
+++ app/users/views.py
@@ -27,7 +27,7 @@ class CreateUserView(APIView):
             return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
         else:
             # Для других статусов можно обработать ошибку по-разному
-            return Response({'error': 'Failed to create user'}, status=status.HTTP_200_OK)
+            return Response({'error': 'Failed to create user'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginUserView(APIView):
@@ -41,8 +41,12 @@ class LoginUserView(APIView):
         response = requests.post(url, params=params)
 
         if response.status_code == 200:
+            data = response.json()
+            token = data["this_user"]["auth_token"]
             user = authenticate(request, username=email, password=password)
             if user is not None:
+                user.token = token
+                user.save()
                 login(request, user)
                 return Response({'message': 'User logged in successfully'}, status=status.HTTP_200_OK)
             else:
@@ -56,13 +60,26 @@ class AuthTokenView(APIView):
         auth_token = request.data.get("auth_token")
         if not auth_token:
             return Response({'error': 'Auth token is required'}, status=status.HTTP_400_BAD_REQUEST)
-
-        user = CustomUser.objects.filter(token=auth_token).first()
-        if not user:
-            return Response({'error': 'Invalid auth token'}, status=status.HTTP_401_UNAUTHORIZED)
-        login(request, user)
-        return Response({'message': 'Auth token verified successfully'}, status=status.HTTP_200_OK)
+        url = "http://80.87.104.189:9991/user/auth"
+        params = {'auth_token': auth_token}
+        response = requests.post(url, params=params)
+        if response.status_code == 200:
+            login(request, request.user)
+            return Response({"message": "Login successfully"}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Auth token verified successfully'}, status=status.HTTP_200_OK)
 
 
 class EmailConfirmationView(APIView):
-    ...
+    def post(self, request):
+        code = request.data.get("code")
+        if not code:
+            return Response({'error': 'Code is required'}, status=status.HTTP_400_BAD_REQUEST)
+        url = "http://80.87.104.189:9991/user/email-confirmation"
+        params = {'code': code}
+        response = requests.post(url, params=params)
+        if response.status_code == 200:
+            login(request, request.user)
+            return Response({'message': 'Email confirmed successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Failed to confirm email'}, status=status.HTTP_200_OK)
